b70f34fa1a39cbf164da6c968a804906b081f0d0
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2021-06-03 16:26:35 +0300

    L04

---
+++ ProgrammingFundamentals/src/L04Arrays/P04ReverseArrayOfStrings.java
@@ -0,0 +1,20 @@
+package L04Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P04ReverseArrayOfStrings {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] stringArr = scan.nextLine().split(" ");
+
+        for (int i = 0; i < stringArr.length / 2; i++) {
+            String buf = stringArr[i];
+            stringArr[i] = stringArr [stringArr.length - 1 - i];
+            stringArr [stringArr.length - 1 - i] = buf;
+        }
+
+        System.out.println(String.join(" ", stringArr));
+
+    }
+}
---
+++ ProgrammingFundamentals/src/L04Arrays/P05EvenAndOddSubtraction.java
@@ -0,0 +1,23 @@
+package L04Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P05EvenAndOddSubtraction {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] numbers = Arrays
+                .stream(scan.nextLine().split(" "))
+                .mapToInt(el->Integer.parseInt(el))
+                .toArray();
+
+        int sumEven = 0;
+        int sumOdd = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) sumEven += numbers[i];
+            else sumOdd += numbers[i];
+        }
+
+        System.out.println(sumEven - sumOdd);
+    }
+}
