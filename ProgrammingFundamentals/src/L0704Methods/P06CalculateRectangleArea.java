1be220bbd767d044f280a6f07685180f41fc32e4
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2021-06-13 18:01:01 +0300

    L08 completed

---
+++ ProgrammingFundamentals/src/L0704Methods/P06CalculateRectangleArea.java
@@ -0,0 +1,20 @@
+package L0704Methods;
+
+import java.lang.reflect.WildcardType;
+import java.util.Scanner;
+
+public class P06CalculateRectangleArea {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int height = Integer.parseInt(scan.nextLine());
+        int width = Integer.parseInt(scan.nextLine());
+
+        int area = calculateArea(height, width);
+
+        System.out.println(area);
+    }
+
+    private static int calculateArea(int height, int width) {
+        return height * width;
+    }
+}
---
+++ ProgrammingFundamentals/src/L0704Methods/P11MathOperations.java
@@ -0,0 +1,38 @@
+package L0704Methods;
+
+import org.w3c.dom.ls.LSOutput;
+
+import javax.crypto.spec.PSource;
+import java.util.Scanner;
+
+public class P11MathOperations {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = Integer.parseInt(scan.nextLine());
+        String operation = scan.nextLine();
+        int b = Integer.parseInt(scan.nextLine());
+
+        System.out.printf("%d", calculate(a, operation, b));
+    }
+
+    private static int calculate(int a, String operation, int b) {
+        int result = 0;
+
+        switch (operation) {
+            case "/":
+                result = a / b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+        }
+
+        return result;
+    }
+}
