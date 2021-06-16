75a32ecbd5bec0147f18b3ca7e80d79d5044b0dd
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2021-06-16 20:57:53 +0300

    Array manipulator last version

--- ProgrammingFundamentals/src/L08MethodsExercise/P11ArrayManipulator.java
+++ ProgrammingFundamentals/src/L08MethodsExercise/P11ArrayManipulator.java
@@ -3,6 +3,7 @@ package L08MethodsExercise;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class P11ArrayManipulator {
     public static void main(String[] args) {
@@ -47,45 +48,41 @@ public class P11ArrayManipulator {
             return;
         }
 
-        StringBuilder sb = new StringBuilder(count * 2 + 2);
+        int[] resultArr = new int[count];
+        int index = 0;
 
         for (int i = 0; i < array.length; i++) {
             if (("even".equals(token) && array[i] % 2 == 0)
                     || ("odd".equals(token) && array[i] % 2 != 0)) {
-                sb.append(array[i]);
-                sb.append(", ");
+                resultArr[index++] = array[i];
+                if (index >= count) break;
             }
         }
-        if (sb.length() > 0)
-            sb.delete(sb.length() - 2, sb.length());
+        resultArr = Arrays.copyOf(resultArr, index);
 
-        System.out.printf("[%s]\n", sb);
+        System.out.println(Arrays.toString(resultArr));
     }
 
     private static void lastElements(int[] array, int count, String token) {
-        StringBuilder sb = new StringBuilder(count * 2 + 2);
-
         if (count > array.length) {
             System.out.println("Invalid count");
             return;
         }
 
-        int ix = -1;
+        int[] resultArr = new int[count];
+        int index = 0;
+
         for (int i = array.length - 1; i >= 0; i--) {
             if (("even".equals(token) && array[i] % 2 == 0)
                     || ("odd".equals(token) && array[i] % 2 != 0)) {
-                ix++;
-                if (ix >= count) break;
-                sb.append(array[i]);
-                sb.append(", ");
+                resultArr[index++] = array[i];
+                if (index >= count) break;
             }
         }
 
-        if (sb.length() > 0)
-            sb.delete(sb.length() - 2, sb.length());
+        resultArr = Arrays.copyOf(resultArr, index);
 
-        System.out.printf("[%s]\n", sb);
-
+        System.out.println(Arrays.toString(resultArr));
     }
 
     private static void minElement(int[] array, String token) {
@@ -93,7 +90,6 @@ public class P11ArrayManipulator {
         int minElementValue = Integer.MAX_VALUE;
 
         for (int i = 0; i < array.length; i++) {
-
             if ((("even".equals(token) && array[i] % 2 == 0) || ("odd".equals(token) && array[i] % 2 != 0))
                     && array[i] <= minElementValue) {
                 minElementIndex = i;
