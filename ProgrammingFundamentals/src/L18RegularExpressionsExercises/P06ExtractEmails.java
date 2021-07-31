5b9473e29b168a0dffd28266ca373c7ea87b267c
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2021-07-31 16:21:53 +0300

    RegEx Excercise

---
+++ ProgrammingFundamentals/src/L18RegularExpressionsExercises/P06ExtractEmails.java
@@ -0,0 +1,23 @@
+package L18RegularExpressionsExercises;
+
+import javax.crypto.Mac;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class P06ExtractEmails {
+    public static final String PATTERN =
+            "\\s(?<email>[A-Za-z0-9]+[\\.\\-\\_]?[A-Za-z0-9]*@[A-Za-z]+[\\-A-Za-z]*(\\.[A-Za-z]+[\\-A-Za-z]*)+)";
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+        Pattern pattern = Pattern.compile(PATTERN);
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()){
+            System.out.println(matcher.group("email"));
+        }
+    }
+}
