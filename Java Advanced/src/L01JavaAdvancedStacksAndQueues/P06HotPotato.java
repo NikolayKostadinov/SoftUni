e625a436d1703ee0eaba3cf2eb188fba91e701d4
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2021-09-14 18:12:03 +0300

    Hot Potatoes

--- Java Advanced/src/L01JavaAdvancedStacksAndQueues/P06HotPotato.java
+++ Java Advanced/src/L01JavaAdvancedStacksAndQueues/P06HotPotato.java
@@ -1,7 +1,22 @@
 package L01JavaAdvancedStacksAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Scanner;
+
 public class P06HotPotato {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayDeque<String> playersQueue = new ArrayDeque<>(List.of(scan.nextLine().split("\\s+")));
+        int number = Integer.parseInt(scan.nextLine());
 
+        while (playersQueue.size() > 1) {
+            for (int i = 0; i < number - 1; i++) {
+                playersQueue.offer(playersQueue.poll());
+            }
+            System.out.printf("Removed %s%n", playersQueue.poll());
+        }
+
+        System.out.printf("Last is %s%n", playersQueue.poll());
     }
 }
