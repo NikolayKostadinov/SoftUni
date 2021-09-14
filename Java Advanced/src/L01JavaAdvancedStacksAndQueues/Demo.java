97176e7f190a00c8bfe33a5adfad0cec9d7710e5
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2021-09-14 16:17:13 +0300

    Stacks & queues

---
+++ Java Advanced/src/L01JavaAdvancedStacksAndQueues/Demo.java
@@ -0,0 +1,15 @@
+package L01JavaAdvancedStacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Stack;
+
+public class Demo {
+    public static void main(String[] args) {
+        // push, pull, peek
+        ArrayDeque<Integer> stack = new ArrayDeque<Integer>();
+
+        // offer, poll , peek
+        ArrayDeque<Integer> queue = new ArrayDeque<Integer>();
+
+    }
+}
---
+++ Java Advanced/src/L01JavaAdvancedStacksAndQueues/P06HotPotato.java
@@ -0,0 +1,7 @@
+package L01JavaAdvancedStacksAndQueues;
+
+public class P06HotPotato {
+    public static void main(String[] args) {
+
+    }
+}
