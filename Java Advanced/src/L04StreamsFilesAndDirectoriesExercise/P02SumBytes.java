d915ecaa152d573cc6426062375c5ab5ed341951
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2021-09-27 10:55:39 +0300

    P04CountCharacterTypes

--- Java Advanced/src/L04StreamsFilesAndDirectoriesExercise/P02SumBytes.java
+++ Java Advanced/src/L04StreamsFilesAndDirectoriesExercise/P02SumBytes.java
@@ -1,7 +1,26 @@
 package L04StreamsFilesAndDirectoriesExercise;
 
-public interface P02SumBytes {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class P02SumBytes {
+    public static void main(String[] args) {
+        Path sourcePath = Paths.get("src/L04StreamsFilesAndDirectoriesExercise/Resources/input.txt");
+
+        try (BufferedReader bufferedReader = Files.newBufferedReader(sourcePath)) {
+            long sumBytes = 0;
+            String line = bufferedReader.readLine();
+            while (line != null){
+                sumBytes += line.chars().sum();
+                line = bufferedReader.readLine();
+            }
+
+            System.out.println(sumBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
