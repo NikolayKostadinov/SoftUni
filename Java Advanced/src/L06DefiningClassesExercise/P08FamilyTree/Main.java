1ae11d0c5b916f0a0c1081f0d10a6effd824fff5
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2021-10-04 22:04:21 +0300

    FamilyTree

--- Java Advanced/src/L06DefiningClassesExercise/P08FamilyTree/Main.java
+++ Java Advanced/src/L06DefiningClassesExercise/P08FamilyTree/Main.java
@@ -1,6 +1,5 @@
 package L06DefiningClassesExercise.P08FamilyTree;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -9,29 +8,12 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Main {
-    private static final String ADD_PATTERN =
-            "^(?<name>(\\w+)\\s(\\w+))\\s(?<birthDay>\\d+\\/\\d+\\/\\d+)$";
-    private static final String PARENT_NAME_CHILD_NAME =
-            "^(?<parentName>(\\w+)\\s(\\w+))\\s-\\s(?<childName>(\\w+)\\s(\\w+))$";
-    private static final String PARENT_NAME_CHILD_BIRTHDAY =
-            "^(?<parentName>(\\w+)\\s(\\w+))\\s-\\s(?<childBirthDay>\\d+\\/\\d+\\/\\d+)$";
-    private static final String PARENT_BIRTHDAY_CHILD_NAME =
-            "^(?<parentBirthDay>\\d+\\/\\d+\\/\\d+)\\s-\\s(?<childName>(\\w+)\\s(\\w+))$";
-    private static final String PARENT_BIRTHDAY_CHILD_BIRTHDAY =
-            "^(?<parentBirthDay>\\d+\\/\\d+\\/\\d+)\\s-\\s(?<childBirthDay>\\d+\\/\\d+\\/\\d+)$";
-
+    private static final String ADD_PATTERN = "^(?<name>(\\w+)\\s(\\w+))\\s(?<birthDay>\\d+\\/\\d+\\/\\d+)$";
     private static final Pattern patternAdd = Pattern.compile(ADD_PATTERN);
-    private static final Pattern parentNameToChildName = Pattern.compile(PARENT_NAME_CHILD_NAME);
-    private static final Pattern patternParentNameToChildBirthDay = Pattern.compile(PARENT_NAME_CHILD_BIRTHDAY);
-    private static final Pattern patternParentBirthDayToChildName = Pattern.compile(PARENT_BIRTHDAY_CHILD_NAME);
-    private static final Pattern patternParentBirthDayToChildBirtDay = Pattern.compile(PARENT_BIRTHDAY_CHILD_BIRTHDAY);
 
     private static Map<String, Person> relativesByDate = new LinkedHashMap<>();
     private static Map<String, Person> relativesByName = new LinkedHashMap<>();
-    private static Map<String, String> parentNameToChildNameCollection = new LinkedHashMap<>();
-    private static Map<String, String> parentNameToChildBirthDayCollection = new LinkedHashMap<>();
-    private static Map<String, String> parentBirthDayToChildNameCollection = new LinkedHashMap<>();
-    private static Map<String, String> parentBirthDayToChildBirtDayCollection = new LinkedHashMap<>();
+    private static Map<String, String> relation = new LinkedHashMap<>();
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -41,7 +23,7 @@ public class Main {
         readInput(scan);
         applyRelations();
         Person person;
-        if (targetPerson.contains("/")){
+        if (targetPerson.contains("/")) {
             person = relativesByDate.get(targetPerson);
         } else {
             person = relativesByName.get(targetPerson);
@@ -69,42 +51,25 @@ public class Main {
     }
 
     private static void applyRelations() {
-        parentNameToChildNameCollection
+        relation
                 .entrySet()
                 .stream()
-                .forEach(entry->{
-                    var parent = relativesByName.get(entry.getKey());
-                    var child = relativesByName.get(entry.getValue());
-                    parent.addChild(child);
-                    child.addParent(parent);
-                });
+                .forEach(entry -> {
+                    Person parent;
+                    Person child;
 
-        parentNameToChildBirthDayCollection
-                .entrySet()
-                .stream()
-                .forEach(entry->{
-                    Person parent = relativesByName.get(entry.getKey());
-                    Person child = relativesByDate.get(entry.getValue());
-                    parent.addChild(child);
-                    child.addParent(parent);
-                });
+                    if (entry.getKey().contains("/")){
+                        parent = relativesByDate.get(entry.getKey());
+                    }else{
+                        parent = relativesByName.get(entry.getKey());
+                    }
 
-        parentBirthDayToChildNameCollection
-                .entrySet()
-                .stream()
-                .forEach(entry->{
-                    Person parent = relativesByDate.get(entry.getKey());
-                    Person child = relativesByName.get(entry.getValue());
-                    parent.addChild(child);
-                    child.addParent(parent);
-                });
+                    if (entry.getValue().contains("/")){
+                        child = relativesByDate.get(entry.getValue());
+                    }else{
+                        child = relativesByName.get(entry.getValue());
+                    }
 
-        parentBirthDayToChildBirtDayCollection
-                .entrySet()
-                .stream()
-                .forEach(entry->{
-                    Person parent = relativesByDate.get(entry.getKey());
-                    Person child = relativesByDate.get(entry.getValue());
                     parent.addChild(child);
                     child.addParent(parent);
                 });
@@ -114,10 +79,6 @@ public class Main {
         String input = scan.nextLine();
         while (!"End".equals(input)) {
             Matcher matcherAdd = patternAdd.matcher(input);
-            Matcher matcherParentNameToChildName = parentNameToChildName.matcher(input);
-            Matcher matcherParentNameToChildBirthDay = patternParentNameToChildBirthDay.matcher(input);
-            Matcher matcherParentBirthDayToChildName = patternParentBirthDayToChildName.matcher(input);
-            Matcher matcherParentBirthDayToChildBirtDay = patternParentBirthDayToChildBirtDay.matcher(input);
 
             if (matcherAdd.find()) {
                 String name = matcherAdd.group("name");
@@ -126,27 +87,11 @@ public class Main {
                 Person person = new Person(name, date);
                 relativesByDate.putIfAbsent(date, person);
                 relativesByName.putIfAbsent(name, person);
-            } else if (matcherParentNameToChildName.find()) {
-                String parentName = matcherParentNameToChildName.group("parentName");
-                String childName = matcherParentNameToChildName.group("childName");
-
-                parentNameToChildNameCollection.putIfAbsent(parentName, childName);
-            } else if (matcherParentNameToChildBirthDay.find()) {
-                String parentName = matcherParentNameToChildBirthDay.group("parentName");
-                String childBirthDay = matcherParentNameToChildBirthDay.group("childBirthDay");
-
-                parentNameToChildBirthDayCollection.putIfAbsent(parentName, childBirthDay);
-            } else if (matcherParentBirthDayToChildName.find()) {
-                String parentBirthDay = matcherParentBirthDayToChildName.group("parentBirthDay");
-                String childName = matcherParentBirthDayToChildName.group("childName");
-
-                parentBirthDayToChildNameCollection.putIfAbsent(parentBirthDay, childName);
-            } else if (matcherParentBirthDayToChildBirtDay.matches()) {
-                String parentBirthDay = matcherParentBirthDayToChildBirtDay.group("parentBirthDay");
-                String childBirthDay = matcherParentBirthDayToChildBirtDay.group("childBirthDay");
-
-                parentBirthDayToChildBirtDayCollection.putIfAbsent(parentBirthDay, childBirthDay);
+            } else {
+                String tokens[] = input.split(" - ");
+                relation.putIfAbsent(tokens[0],tokens[1]);
             }
+
             input = scan.nextLine();
         }
     }
