d6b1d65d5828a40be1453f1f93b67deea53f1ce1
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2021-10-05 18:57:55 +0300

    groomingSalon

--- Java Advanced/src/L06DefiningClassesExercise/P08FamilyTree/Person.java
+++ Java Advanced/src/L06DefiningClassesExercise/P08FamilyTree/Person.java
@@ -1,9 +1,7 @@
 package L06DefiningClassesExercise.P08FamilyTree;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Person {
     private String name;
