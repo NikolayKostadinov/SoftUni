1e597b86bcf44b15ba2f407faaf5df3b426d31f4
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2021-10-14 19:44:27 +0300

    P09E Finished

---
+++ Java Advanced/src/L09IteratorsAndComparatorsExercise/P09LinkedListTraversal/LinkedListIterator.java
@@ -0,0 +1,25 @@
+package L09IteratorsAndComparatorsExercise.P09LinkedListTraversal;
+
+import java.util.Iterator;
+
+public class LinkedListIterator implements Iterator<Integer> {
+    private final LinkedList list;
+    private Node currentNode;
+
+    public LinkedListIterator(LinkedList list) {
+        this.list = list;
+        currentNode = this.list.getHead();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return this.currentNode != null;
+    }
+
+    @Override
+    public Integer next() {
+        int value = this.currentNode.value;
+        this.currentNode = this.currentNode.getNext();
+        return value;
+    }
+}
