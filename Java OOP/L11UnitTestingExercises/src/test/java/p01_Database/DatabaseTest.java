cdddb034b9843b2f22b9d277078a4e9b4db261b7
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2021-11-26 15:42:09 +0200

    02 Java OOP Exam - 15 August 2021

--- Java OOP/L11UnitTestingExercises/src/test/java/p01_Database/DatabaseTest.java
+++ Java OOP/L11UnitTestingExercises/src/test/java/p01_Database/DatabaseTest.java
@@ -1,5 +1,6 @@
 package p01_Database;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import javax.naming.OperationNotSupportedException;
@@ -10,13 +11,19 @@ import static org.junit.Assert.*;
 
 public class DatabaseTest {
 
-    private static final Integer[] ELEMENTS = new Integer[]{1, 2, 3};
+    private static final Integer[] ELEMENTS = {1, 2, 3};
+    public static final Integer[] ELEMENTS17 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+    private static final Integer[] ELEMENTS0 = {};
+
+    private Database db;
+
+    @Before
+    public void Initialize() throws OperationNotSupportedException {
+        db = new Database(ELEMENTS);
+    }
 
     @Test
-    public void ctorCreatesDatabase() throws OperationNotSupportedException {
-        // Arrange & Act
-        Database db = new Database(ELEMENTS);
-
+    public void ctorCreatesDatabase() {
         // Assert
         Integer[] result = db.getElements();
         assertArrayEquals(ELEMENTS, result);
@@ -25,45 +32,39 @@ public class DatabaseTest {
     @Test(expected = OperationNotSupportedException.class)
     public void ctorThrowsExceptionIfElementsCountIs0() throws OperationNotSupportedException {
         // Arrange & Act & Assert
-        new Database();
+        new Database(ELEMENTS0);
     }
 
     @Test(expected = OperationNotSupportedException.class)
     public void ctorThrowsExceptionIfElementsCountIs17() throws OperationNotSupportedException {
         // Arrange & Act & Assert
-        new Database(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17);
+        new Database(ELEMENTS17);
     }
 
     @Test
     public void addAnElementAtTheNextFreeCell() throws OperationNotSupportedException {
-        // Arrange
-        Database db = new Database(ELEMENTS);
         // Act
-        db.add(2);
+        db.add(10);
         // Assert
-        assertEquals(ELEMENTS.length + 1, db.getElements().length);
+        Integer[] result = db.getElements();
+        assertEquals(ELEMENTS.length + 1, result.length);
+        assertEquals(Integer.valueOf(10), result[result.length-1] );
     }
 
     @Test(expected = OperationNotSupportedException.class)
     public void addNullElementThrowsException() throws OperationNotSupportedException {
-        // Arrange
-        Database db = new Database(ELEMENTS);
         // Act & Assert
         db.add(null);
     }
 
     @Test
     public void removeRemovesElementAtTheLastIndex() throws OperationNotSupportedException {
-        // Arrange
-        Database db = new Database(ELEMENTS);
-
         // Act
         db.remove();
 
-
         // Assert
         Integer[] result = db.getElements();
-        assertArrayEquals(Arrays.copyOf(ELEMENTS,ELEMENTS.length-1), result);
+        assertArrayEquals(Arrays.copyOf(ELEMENTS, ELEMENTS.length - 1), result);
     }
 
     @Test(expected = OperationNotSupportedException.class)
