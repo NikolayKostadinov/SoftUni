e2e63a048c7cd4310a79cb34cdc7d855a181899c
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2022-02-10 13:34:02 +0200

    DSF E01

--- Data Structures Fundamentals/01. LinearDataStructures/01. Faster Queue_Skeleton/src/main/java/Main.java
+++ Data Structures Fundamentals/01. LinearDataStructures/01. Faster Queue_Skeleton/src/main/java/Main.java
@@ -2,17 +2,74 @@ import implementations.ArrayDeque;
 
 public class Main {
     public static void main(String[] args) {
-        var test = new ArrayDeque<Integer>();
-        test.add(1);
-        test.add(2);
-        test.add(3);
-        test.add(5);
-        test.add(6);
-        test.add(7);
-        test.insert(3, 4);
-        test.insert(0, 0);
-        test.add(9);
-        test.insert(8,8);
+        var test = new ArrayDeque<String>();
+        test.add("1");
+        System.out.println(test);
+        test.add("2");
+        System.out.println(test);
+        test.add("3");
+        System.out.println(test);
+        test.add("5");
+        System.out.println(test);
+        test.add("6");
+        System.out.println(test);
+        test.add("7");
+        System.out.println(test);
+        test.insert(3, "4");
+        System.out.println(test);
+        test.insert(0, "0");
+        System.out.println(test);
+        test.add("9");
+        System.out.println(test);
+        test.insert(8,"8");
+        System.out.println(test);
         test.remove(3);
+        System.out.println(test);
+        test.trimToSize();
+        System.out.println(test);
+        test.set(0,"10");
+        System.out.println(test);
+        test.set(5,"666");
+        System.out.println(test);
+        test.remove(5);
+        System.out.println(test);
+        test.insert(5,"6");
+        System.out.println(test);
+        test.insert(3,"3");
+        System.out.println(test);
+        test.set(0,"0");
+        System.out.println(test);
+
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < 10; i++) {
+            queue.offer(i);
+        }
+        print(queue);
+        System.out.println("peek -> " + queue.peek());
+
+        while (!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < 10; i++) {
+            stack.push(i);
+        }
+        print(stack);
+        System.out.println("peek -> " + stack.peek());
+
+        while (!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
+
+    }
+
+    private static void print(ArrayDeque<Integer> test) {
+        StringBuilder sb = new StringBuilder();
+        for (Integer element : test) {
+            sb.append(element).append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
     }
 }
--- Data Structures Fundamentals/01. LinearDataStructures/01. Faster Queue_Skeleton/src/main/java/implementations/ArrayDeque.java
+++ Data Structures Fundamentals/01. LinearDataStructures/01. Faster Queue_Skeleton/src/main/java/implementations/ArrayDeque.java
@@ -1,41 +1,38 @@
 package implementations;
 
 import interfaces.Deque;
-import interfaces.LinkedList;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.stream.Collectors;
 
 public class ArrayDeque<E> implements Deque<E> {
 
     private final int DEFAULT_CAPACITY = 7;
     private int head;
-    private int tail;
     private int size;
 
     private Object[] elements;
 
     public ArrayDeque() {
         this.elements = new Object[DEFAULT_CAPACITY];
-        this.head = getZeroElement();
-        this.tail = this.head;
+        this.head = this.DEFAULT_CAPACITY / 2;
+    }
+
+    public int getLeftTail() {
+        return this.head - size;
+    }
+
+    public int getRightTail() {
+        return this.head + size;
     }
 
     @Override
     public void add(E element) {
         ensureCapacity();
-        this.elements[head--] = element;
-        this.elements[tail++] = element;
+        this.elements[getLeftTail()] = element;
+        this.elements[getRightTail()] = element;
         size++;
-    }
-
-    @Override
-    public void offer(E element) {
-        add(element);
-    }
-
-    @Override
-    public void addFirst(E element) {
-        insert(0, element);
     }
 
     @Override
@@ -45,81 +42,102 @@ public class ArrayDeque<E> implements Deque<E> {
 
     @Override
     public void push(E element) {
-        addLast(element);
+        add(element);
+    }
+
+    @Override
+    public void offer(E element) {
+        add(element);
+    }
+
+
+    @Override
+    public void addFirst(E element) {
+        insert(0, element);
     }
 
     @Override
     public void insert(int index, E element) {
         checkIndex(index);
         ensureCapacity();
-        int leftMostElementIndex = getZeroElement() - size + 1;
-        System.arraycopy(this.elements, leftMostElementIndex, this.elements, leftMostElementIndex - 1, size - index);
-        int rightStartElementIndex = getZeroElement() + index;
+        System.arraycopy(this.elements, getLeftTail() + 1, this.elements, getLeftTail() , size - index);
+        int rightStartElementIndex = this.head + index;
         System.arraycopy(this.elements, rightStartElementIndex, this.elements, rightStartElementIndex + 1, size - index);
-        this.elements[getZeroElement() - index] = element;
-        this.elements[getZeroElement() + index] = element;
-        increaseSize();
+        this.elements[this.head - index] = element;
+        this.elements[this.head + index] = element;
+        size++;
     }
 
     @Override
     public void set(int index, E element) {
         checkIndex(index);
-        this.elements[getZeroElement() - index] = element;
-        this.elements[getZeroElement() + index] = element;
+        this.elements[this.head - index] = element;
+        this.elements[this.head + index] = element;
     }
 
     @Override
     public E peek() {
-        return get(size == 0 ? null : elements[this.head]);
+        return get(size-1);
     }
 
     @Override
     public E poll() {
-        return null;
+        return removeFirst();
     }
 
     @Override
     public E pop() {
-        return null;
+        return removeLast();
     }
 
     @Override
+    @SuppressWarnings("unchecked cast")
     public E get(int index) {
-        return null;
+        checkIndex(index);
+        return (E) this.elements[this.head + index];
     }
 
     @Override
+    @SuppressWarnings("unchecked cast")
     public E get(Object object) {
-        return null;
+        return Arrays.stream(this.elements)
+                .limit(this.elements.length / 2)
+                .filter(e -> e.equals(object))
+                .map(e -> (E) e)
+                .findFirst().orElse(null);
     }
 
     @Override
     public E remove(int index) {
         checkIndex(index);
-        E element = (E) this.elements[getZeroElement() + index];
-        int leftMostElementIndex = getZeroElement() - size + 1;
-        System.arraycopy(this.elements, leftMostElementIndex, this.elements, leftMostElementIndex + 1, size - index - 1);
-        int rightStartElementIndex = getZeroElement() + index + 1;
-        System.arraycopy(this.elements, rightStartElementIndex, this.elements, rightStartElementIndex - 1, size - index - 1);
-        this.elements[this.head] = null;
-        this.elements[this.tail] = null;
-        decreaseSize();
+        E element = (E) this.elements[this.head + index];
+        System.arraycopy(this.elements, getLeftTail()+1, this.elements, getLeftTail() + 2, size - index-1);
+        int rightStartElementIndex = this.head + index + 1;
+        System.arraycopy(this.elements, rightStartElementIndex, this.elements, rightStartElementIndex - 1, size - index-1);
+        this.elements[getLeftTail() + 1 ] = null;
+        this.elements[getRightTail() - 1 ] = null;
+        size--;
         return element;
     }
 
     @Override
     public E remove(Object object) {
+        for (int i = this.head; i > this.getLeftTail(); i--) {
+            if (elements[i].equals(object)) {
+                return remove(this.head - i);
+            }
+        }
         return null;
     }
 
     @Override
     public E removeFirst() {
-        return this.remove(getZeroElement());
+        return this.remove(0);
     }
 
     @Override
     public E removeLast() {
-        return null;
+        return this.remove(size-1);
     }
 
     @Override
@@ -129,57 +147,61 @@ public class ArrayDeque<E> implements Deque<E> {
 
     @Override
     public int capacity() {
-        return 0;
+        return this.elements.length / 2 + 1;
     }
 
     @Override
     public void trimToSize() {
-
+        int leftElementIndex = this.head - (size - 1);
+        Object[] newElements = new Object[size*2-1];
+        System.arraycopy(this.elements,leftElementIndex, newElements, 0, newElements.length);
+        this.elements = newElements;
+        this.head = newElements.length / 2;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            int position = 0;
+            @Override
+            public boolean hasNext() {
+                return this.position < size;
+            }
+
+            @Override
+            public E next() {
+                return get(this.position++);
+            }
+        };
     }
 
     private void ensureCapacity() {
-        if (this.size == (getZeroElement()) + 1) {
+        if (this.size == this.capacity()) {
             grow();
         }
     }
 
+    @Override
+    public String toString() {
+       return Arrays.stream(this.elements)
+               .map(x->x == null ? "_":x.toString())
+               .collect(Collectors.joining(" "));
+    }
+
     private void grow() {
         Object[] newElements = new Object[this.elements.length * 2 + 1];
-        int zeroElementIndex = newElements.length / 2;
-        System.arraycopy(this.elements, 0, newElements, zeroElementIndex - size + 1, this.elements.length);
-        this.head = zeroElementIndex - size;
-        this.tail = zeroElementIndex + size;
+        System.arraycopy(this.elements, 0, newElements, (newElements.length / 2 + 1) - size, this.elements.length);
+        this.head = newElements.length / 2;
         this.elements = newElements;
     }
 
     private void checkIndex(int index) {
         if (0 > index || index > size - 1)
             throw new IndexOutOfBoundsException();
-    }
-
-    private void increaseSize() {
-        head--;
-        tail++;
-        size++;
-    }
-
-    private void decreaseSize() {
-        this.head++;
-        this.tail--;
-        this.size--;
-    }
-
-    private int getZeroElement() {
-        return this.elements.length / 2;
     }
 }
